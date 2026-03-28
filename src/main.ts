dacc96deacfd6c4bf677456f6cb2ee06fa416b26
joniashimi10 <jashimi@rpay.ai>
2026-03-28 18:18:43 +0100

    DB Connection, update package.json and give env.example for setup

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,32 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { BadRequestException, ValidationPipe } from '@nestjs/common';
+
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  app.useGlobalPipes(
+      new ValidationPipe({
+        whitelist: false,
+        forbidNonWhitelisted: false,
+        transform: true,
+        exceptionFactory: (errors) => {
+          return new BadRequestException(errors);
+        },
+      }),
+  );
+
+
+  app.enableCors({
+    origin: '*',
+    methods: 'GET,POST,PUT,PATCH,DELETE,OPTIONS',
+    allowedHeaders: 'Content-Type, Authorization',
+  });
+
+
+  const port = process.env.PORT || 3000;
+  await app.listen(port);
+  console.log(`Running on port ${port}`);
 }
 bootstrap();
